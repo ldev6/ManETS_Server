7edbeba14aec9eaec3c82d70af980a6a2246dc90
Philippe <pdavid@solutionsmedias360.com>
2013-10-09 23:39:32 -0400

    json tests

--- src/ca/etsmtl/gti785/ServletManETS.java
+++ src/ca/etsmtl/gti785/ServletManETS.java
@@ -1,17 +1,22 @@
 package ca.etsmtl.gti785;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
-
 import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+import ca.etsmtl.gti785.model.DataSource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
 
@@ -20,15 +25,36 @@ import com.sun.jna.NativeLibrary;
  */
 public class ServletManETS extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String hostAddress;
+	private static String userHome;
+	private static String musicHome;
+	private static String videoHome;
 
-
-	//EXECUTED AT START
+	// EXECUTED AT START
 	static {
-		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "C:\\Program Files\\VideoLAN\\VLC");
+		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(),
+				"C:\\Program Files\\VideoLAN\\VLC");
 		System.out.println("C:\\Program Files\\VideoLAN\\VLC");
-		
+
 		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
 		System.out.println("Native Lib loaded");
+
+		userHome = System.getProperty("user.home");
+		musicHome = checkMusicHomeExist();
+		videoHome = checkMusicVideoExist();
+
+		System.out.println(userHome);
+
+		InetAddress IP = null;
+		try {
+			IP = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		hostAddress = IP.getHostAddress();
+		hostAddress += ":8080/ManETS_Server/#";
+		System.out.println("IP of my system is := " + hostAddress);
 	}
 
 	/**
@@ -36,26 +62,41 @@ public class ServletManETS extends HttpServlet {
 	 */
 	public ServletManETS() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
+	private static String checkMusicVideoExist() {
+		return new File(userHome + "/Music").exists() ? userHome + "/Music"
+				: "false";
+	}
+
+	private static String checkMusicHomeExist() {
+		return new File(userHome + "/Video").exists() ? userHome + "/Music"
+				: "false";
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json");
-		response.getWriter().write("{dataSources:[music:/music/,video:/video/]}");
+
+		DataSource r = new DataSource("root", hostAddress);
+		DataSource m = new DataSource("musicHome", musicHome);
+		DataSource v = new DataSource("videoHome", videoHome);
+
+//		response.getWriter().write("{\"dataSources\":[\"music\",\"video\"]}");
+		response.getWriter().write(new ObjectMapper().writeValueAsString(r));
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
--- src/ca/etsmtl/gti785/model/PlayList.java
+++ src/ca/etsmtl/gti785/model/PlayList.java
@@ -1,7 +1,10 @@
 package ca.etsmtl.gti785.model;
 
+import java.util.List;
+
 /**
  * Created by Phil on 24/09/13.
  */
 public class PlayList extends Feed {
+	List<Media> media;
 }
